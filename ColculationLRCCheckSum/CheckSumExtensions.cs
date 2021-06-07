9bbff478fe11b95e5c74c5d8b36a765b8cc9b33f
mansur <kurtov.mansur@gmail.com>
2021-06-07 16:57:26 +0500

    Add project files.

---
+++ ColculationLRCCheckSum/CheckSumExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ColculationLRCCheckSum
+{
+    internal static class CheckSumExtensions
+    {
+        internal static byte[] GetLRC(this byte[] nums)
+        {
+
+            byte current = 0x00;
+            foreach (var value in nums)
+            {
+                current ^= value;
+            }
+
+            return current.ByteToLRCByteArray();
+        }
+        private static byte[] ByteToLRCByteArray(this byte data)
+        {
+            var resultData = new byte[2];
+            var byteString = BitConverter.ToString(new byte[] { data });
+
+            for (int i = 0; i < byteString.Length; i++)
+                resultData[i] = Convert.ToByte(byteString[i]);
+
+            return resultData;
+        }
+    }
+}
---
+++ ColculationLRCCheckSum/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ColculationLRCCheckSum
+{
+    /// <summary>
+    /// Colculation LRC check sum
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new byte[] { 0x30, 0x30, 0x34, 0x35, 0x34, 0x30, 0x34, 0x03 };
+            var result = data.GetLRC(); // 0x33, 0x32
+        }
+    }
+}
